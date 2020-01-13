6103aec645b8b9def3482c63229ecc5618687b8b
Puzant <PBAKJEJIAN@hotmail.com>
2020-01-13 17:19:10 +0200

    sales chart component

---
+++ src/components/SalesChart.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .sales-chart-contaienr
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="sass">
+.sales-chart-contaienr
+  margin-left: 265px
+  margin-top: 20px
+  width: 772px
+  height: 484px
+  border-radius: 5px
+  background-color: #212936
+
+  
+</style>
