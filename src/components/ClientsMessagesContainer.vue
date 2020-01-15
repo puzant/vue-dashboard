53dccb6817c153e8838938038fea8a0ca8e3815b
Puzant <PBAKJEJIAN@hotmail.com>
2020-01-15 13:33:20 +0200

    wrapper component

---
+++ src/components/ClientsMessagesContainer.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .clients-messages-container
+    Clients
+    Messages
+
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Clients from '@/components/Clients.vue';
+import Messages from '@/components/Messages.vue';
+
+@Component({
+  components: {
+    Clients,
+    Messages
+  }
+})
+export default class ClientsMessagesContiner extends Vue {
+
+}
+</script>
+
+<style lang="sass">
+.clients-messages-container
+  display: flex
+  flex-direction: row
+  align-items: center
+</style>
