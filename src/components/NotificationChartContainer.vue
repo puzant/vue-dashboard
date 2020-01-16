cb798bfda04f67ea41fd0beb6951a48d6ba7e0ba
Puzant <PBAKJEJIAN@hotmail.com>
2020-01-16 19:02:13 +0200

    create wrapper component for Notification & Chart components

---
+++ src/components/NotificationChartContainer.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .notification-chart-container
+    Notifications
+    SalesChart
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Notifications from '@/components/Notifications.vue';
+import SalesChart from '@/components/SalesChart.vue';
+
+@Component({
+  components: {
+    Notifications,
+    SalesChart
+  }
+})
+export default class NotificationContainer extends Vue {
+
+}
+
+</script>
+
+<style lang="sass">
+.notification-chart-container
+  display: flex 
+  flex-direction: column
+</style>
